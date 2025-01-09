fd64fa7e79fe7f675543b19d73f6dd2538d22d64
Leandro Falcão <falcao@falcaotd.com.br>
2025-01-09 18:34:31 -0300

    add database_cleaner_spec

--- Gemfile
+++ Gemfile
@@ -52,4 +52,5 @@ group :test do
   gem "rspec-rails"
   gem "factory_bot_rails"
   gem "shoulda-matchers"
+  gem "database_cleaner"
 end
--- spec/factories/legacy_order_import.rb
+++ spec/factories/legacy_order_import.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :legacy_order_import do
     client
-    file { File.open("#{Rails.root}/public/desafio/data_1.txt") }
+    file { Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, "/public/desafio/data_1.txt"))) }
   end
 end
